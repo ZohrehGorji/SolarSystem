7407d477266b13593594ad636e974957d035fa55
Zohreh Gorji <e1129437@student.tuwien.ac.at>
2023-02-18 12:40:17 +0100

    add swagger API

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const mongoString = process.env.DATABASE_URL;
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger_output.json')
+
 mongoose.connect(mongoString);
 const database = mongoose.connection;
 
@@ -19,9 +22,15 @@ app.use(cors())
 app.use(express.json());
 
 const routes = require('./routes/routes');
-
-app.use('/api', routes)
-
 app.listen(3000, () => {
     console.log(`Server Started at ${3000}`)
 })
+app.use('/api', routes)
+app.use(
+    '/doc',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerFile)
+);
+
+
+
